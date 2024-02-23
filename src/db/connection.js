6ff0eedd8fdb148725b11f2d772370fad0e382eb
KatelynnMM <145621288+KatelynnMM@users.noreply.github.com>
2024-02-22 21:26:55 -0500

    secure password

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,11 +1,12 @@
 // connection.js
 const mysql = require('mysql2');
+require('dotenv').config(); // Load environment variables from .env file
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'your_username',
-    password: 'your_password',
-    database: 'company_db'
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'your_username',
+    password: process.env.DB_PASSWORD || 'your_password',
+    database: process.env.DB_DATABASE || 'company_db',
 });
 
 module.exports = connection;
